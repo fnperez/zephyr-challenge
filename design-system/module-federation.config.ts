65cc1797f52c03df1a5fbebd7aacf4bca705c86d
Facundo Perez <facundo@devmates.io>
2025-07-03 16:24:19 -0300

    First commit

---
+++ design-system/module-federation.config.ts
@@ -0,0 +1,5 @@
+export const mfConfig = {
+  name: "design_system",
+  exposes: {},
+  shared: ["react", "react-dom"],
+};
---
+++ store/module-federation.config.ts
@@ -0,0 +1,5 @@
+export const mfConfig = {
+  name: "store",
+  exposes: {},
+  shared: ["react", "react-dom"],
+};
